c5f4157dbc58ef8bd360831836f71a71d7732b55
sfburaktrn <sbtorun.sbt@gmail.com>
2023-09-30 17:10:56 +0300

    bir çok şey yapıldı

--- proje/src/App.js
+++ proje/src/App.js
@@ -3,46 +3,13 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import IndexSlider from "./Pages/Slider.js";
 import Uyekayit from "./Pages/Uyekayit.js";
 import English from "./Pages/English.js";
+import Taslak from "./Pages/Slider2.js";
 
 function App() {
-  const [areButtonsVisible, setButtonsVisibility] = useState(true);
-
-  const handleButtonClick = () => {
-    setButtonsVisibility(false);
-  };
-
   return (
-    <Router>
-      <div className="flex flex-col items-center justify-center h-screen">
-        {areButtonsVisible && (
-          <>
-            <button
-              className="btn border  bg-blue-500"
-              onClick={handleButtonClick}
-            >
-              <Link to="/IndexSlider">Go to IndexSlider Page</Link>
-            </button>
-            <button
-              className="btn border  bg-blue-500 mt-5"
-              onClick={handleButtonClick}
-            >
-              <Link to="/Uyekayit">Go to Uyekayit Page</Link>
-            </button>
-            <button
-              className="btn border  bg-blue-500 mt-5"
-              onClick={handleButtonClick}
-            >
-              <Link to="/English">Go to English Page</Link>
-            </button>
-          </>
-        )}
-        <Routes>
-          <Route path="/IndexSlider" element={<IndexSlider />} />
-          <Route path="/Uyekayit" element={<Uyekayit />} />
-          <Route path="/English" element={<English />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Taslak />
+    </div>
   );
 }
 
---
+++ proje/src/Pages/Slider2.js
@@ -0,0 +1,243 @@
+import React, { useState, useEffect } from "react";
+import svgBaslik from "../Svg/baslik.svg";
+import Istanbul from "../Image2/Layer 75.png";
+import Istanbulmor from "../Image2/Layer 71 mor.png";
+import yazihead from "../Image2/Layer 74.png";
+import svgcomputer from "../Svg/computer.svg";
+import svgtakvim from "../Svg/takvim.svg";
+import svgkiz from "../Svg/kiz.svg";
+import svgtarih from "../Svg/tarih.svg";
+import svgkalp from "../Svg/kalp.svg";
+import svgkaydetme from "../Svg/kaydetme.svg";
+import svggönderme from "../Svg/gönderme.svg";
+
+const sliderContent = [
+  {
+    image: Istanbul,
+    overlayImage: Istanbulmor,
+    headerImage: yazihead,
+    title: "PARTNER ŞİDDETİ PANEL-WORKSHOP 1",
+    description: "İSTANBUL'DA YÜZ YÜZE VE ONLINE KATILIMLA 1",
+  },
+  {
+    image: Istanbul,
+    overlayImage: Istanbulmor,
+    headerImage: yazihead,
+    title: "PARTNER ŞİDDETİ PANEL-WORKSHOP 2",
+    description: "İSTANBUL'DA YÜZ YÜZE VE ONLINE KATILIMLA 2",
+  },
+  {
+    image: Istanbul,
+    overlayImage: Istanbulmor,
+    headerImage: yazihead,
+    title: "PARTNER ŞİDDETİ PANEL-WORKSHOP 3",
+    description: "İSTANBUL'DA YÜZ YÜZE VE ONLINE KATILIMLA 1",
+  },
+  {
+    image: Istanbul,
+    overlayImage: Istanbulmor,
+    headerImage: yazihead,
+    title: "PARTNER ŞİDDETİ PANEL-WORKSHOP 4",
+    description: "İSTANBUL'DA YÜZ YÜZE VE ONLINE KATILIMLA 1",
+  },
+];
+
+function Slider2() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % sliderContent.length);
+    }, 3000); // Geçiş 3 saniyede
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const handleDotClick = (index) => {
+    setCurrentSlide(index);
+  };
+
+  return (
+    <section className="h-screen flex flex-col">
+      <header>
+        <div className="flex justify-end mr-4 mt-3">
+          <a href="register.html" className="btn btn-primary">
+            Üye OL
+          </a>
+          <div className="border-r border-black h-6 mx-2"></div>
+          <a href="login.html" className="btn btn-primary">
+            Giriş Yap
+          </a>
+          <div className="border-r border-black h-6 mx-2"></div>
+          <a href="online-certificates.html" className="btn btn-primary">
+            Online Sertifika Al
+          </a>
+        </div>
+        <img className="mx-auto mb-4 w-auto" src={svgBaslik} alt="Logo" />
+        <div className="flex flex-wrap  justify-center  ">
+          <a href="/services" className=" ml-auto m-5 text-xl font-semibold ">
+            HİZMETLERİMİZ
+          </a>
+          <a href="/articles" className=" m-5 text-xl font-semibold">
+            MAKALELER
+          </a>
+          <a href="/about" className=" m-5 text-xl font-semibold">
+            HAKKIMIZDA
+          </a>
+          <a href="/calendar" className=" m-5 text-xl font-semibold">
+            EĞİTİM TAKVİMİ
+          </a>
+          <a href="/contact" className=" m-5 text-xl font-semibold">
+            İLETİŞİM
+          </a>
+          <div className="mx-16">
+            <input
+              type="text"
+              placeholder="Search"
+              className="  h-8 border-collapse w-72  bg-gray-300 rounded-2xl "
+            />
+          </div>
+        </div>
+      </header>
+      <div className="relative flex items-center justify-center">
+        <img
+          className="w-full h-auto"
+          src={sliderContent[currentSlide].image}
+          alt="mainPhoto"
+        />
+        <img
+          className="absolute top-0 left-0 w-full h-auto"
+          src={sliderContent[currentSlide].overlayImage}
+          alt="overlay"
+        />
+        <div className="absolute w-80 sm:w-96 md:w-120 lg:w-160 xl:w-192 h-auto flex items-center justify-center">
+          <img
+            src={sliderContent[currentSlide].headerImage}
+            alt="centeredImage"
+          />
+        </div>
+        <div className="absolute inset-x-0 bottom-14 text-center hidden md:block sm:block ">
+          <span className="font-semibold text-4xl text-white  ">
+            {sliderContent[currentSlide].title}
+          </span>
+          <span className="font-thin text-white text-base block">
+            {sliderContent[currentSlide].description}
+          </span>
+          <div className="flex justify-center mt-4">
+            {sliderContent.map((slide, index) => (
+              <div
+                key={index}
+                onClick={() => handleDotClick(index)}
+                className={`w-2 h-2 mx-2 bottom-14 rounded-full border border-white cursor-pointer ${
+                  index === currentSlide ? "bg-white" : "bg-transparent"
+                }`}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <main>
+        <div className="bg-zinc-700 p-4">
+          <div className="flex justify-center items-center flex-wrap">
+            <div className="w-full sm:w-1/2 md:w-1/3 p-4">
+              <div className="flex lg:flex-row sm:flex-col md:flex-col bg-gray-400 justify-between items-center py-4 px-4 h-44">
+                <p className="mr-2 text-3xl font-extralight">
+                  English Education <br /> Sales Page
+                </p>
+                <img className="w-32 h-28" src={svgcomputer} alt="computer" />
+              </div>
+            </div>
+
+            <div className="w-full sm:w-1/2 md:w-1/3 p-4">
+              <div className="flex lg:flex-row sm:flex-col md:flex-col bg-gray-400 justify-between items-center py-4 px-4 h-44">
+                <p className="mr-2 text-3xl font-extralight ">Eğitim Takvimi</p>
+                <img className="w-32 h-28" src={svgtakvim} alt="takvim" />
+              </div>
+            </div>
+
+            <div className="w-full sm:w-1/2 md:w-1/3 p-4">
+              <div className="flex lg:flex-row sm:flex-col md:flex-col bg-[#DDDDDD] justify-between items-center py-4 px-4 h-44">
+                <p className="mr-2">
+                  Çoktan
+                  <br /> Seçmeli
+                  <br /> Online
+                  <br /> Testler
+                </p>
+                <img className="w-32 h-28" src={svgkiz} alt="kiz" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#EC9D36] ">
+          <div className="flex justify-center flex-wrap space-x-16 pt-2 ">
+            <p className="bg-amber-800 rounded-3xl w-auto text-black p-4 text-center px-8 mb-4 flex-1 md:flex-none md:mb-0">
+              GELECEK EĞİTİMLERİMİZ
+            </p>
+            <p className="bg-emerald-400 rounded-3xl w-auto text-gray-50 p-4 text-center px-8 mb-4 flex-1 md:flex-none md:mb-0">
+              ÖNCEKİ EĞİTİMLERİMİZ
+            </p>
+          </div>
+          <div className="flex flex-col md:flex-row md:justify-center pt-3">
+            <div className="w-96 h-44 flex lg:flex-row sm:flex-col md:flex-col bg-[#F8B6A2] justify-between items-center py-4 px-4  mr-6 relative ">
+              <p className="text-left">Sensorimotor Travma Terapisi Eğitimi</p>
+              <div className="absolute right-0 top-0 bottom-0 bg-white flex flex-col justify-center items-end">
+                <img className="w-8 h-8 mb-2" src={svgtarih} alt="Image 1" />
+                <img className="w-8 h-8 mb-2" src={svgkalp} alt="Image 2" />
+                <img className="w-8 h-8 mb-2" src={svgkaydetme} alt="Image 3" />
+                <img className="w-8 h-8 mb-2" src={svggönderme} alt="Image 4" />
+              </div>
+            </div>
+            <div className="w-96 flex lg:flex-row sm:flex-col md:flex-col bg-[#40BDB7]  justify-between items-center py-4 px-4 h-44 mr-6 relative">
+              <p className="text-left">
+                Nilüfer Deveciğil İle Oyun Terapisinde Aile İle Çalışmak
+              </p>
+              <div className="absolute right-0 top-0 bottom-0 bg-white flex flex-col justify-center items-end">
+                <img className="w-8 h-8 mb-2" src={svgtarih} alt="Image 1" />
+                <img className="w-8 h-8 mb-2" src={svgkalp} alt="Image 2" />
+                <img className="w-8 h-8 mb-2" src={svgkaydetme} alt="Image 3" />
+                <img className="w-8 h-8 mb-2" src={svggönderme} alt="Image 4" />
+              </div>
+            </div>
+            <div className="w-96 flex lg:flex-row sm:flex-col md:flex-col bg-[#DC80B7]  justify-between items-center py-4 px-4 h-44 mr-6 relative">
+              <p className="text-left">
+                Online:Deneyimsel Oyun Terapisi 1.Düzey Eğitimi
+              </p>
+              <div className="absolute right-0 top-0 bottom-0 bg-white flex flex-col justify-center items-end">
+                <img className="w-8 h-8 mb-2" src={svgtarih} alt="Image 1" />
+                <img className="w-8 h-8 mb-2" src={svgkalp} alt="Image 2" />
+                <img className="w-8 h-8 mb-2" src={svgkaydetme} alt="Image 3" />
+                <img className="w-8 h-8 mb-2" src={svggönderme} alt="Image 4" />
+              </div>
+            </div>
+            <div className="w-96 flex lg:flex-row sm:flex-col md:flex-col bg-[#95B1BE]  justify-between items-center py-4 px-4 h-44 mr-6 relative">
+              <p className="text-left">
+                Gottman Çift Terapisi 1. Düzey Eğitimi
+              </p>
+              <div className="absolute right-0 top-0 bottom-0 bg-white flex flex-col justify-center items-end">
+                <img className="w-8 h-8 mb-2" src={svgtarih} alt="Image 1" />
+                <img className="w-8 h-8 mb-2" src={svgkalp} alt="Image 2" />
+                <img className="w-8 h-8 mb-2" src={svgkaydetme} alt="Image 3" />
+                <img className="w-8 h-8 mb-2" src={svggönderme} alt="Image 4" />
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center justify-center pt-3 flex-col">
+            <p className="bg-amber-800 rounded-3xl w-auto text-white p-2 text-center px-8 mb-2">
+              TARİH SEÇİNİZ Ağustos 2022
+            </p>
+            <div className="flex items-center justify-center w-16 bg-white">
+              <span className="text-[#EC9D36] text-xl font-bold">
+                {" "}
+                &lt; &gt;{" "}
+              </span>
+            </div>
+          </div>
+        </div>
+      </main>
+    </section>
+  );
+}
+
+export default Slider2;
